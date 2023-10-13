1daee9d4ea4e1563fe8420a9036cc02cb9170746
Laura Kajpust <laura@joinviolet.com>
2023-10-13 11:29:57 -0500

    Try seeing if its the download feature

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,4 +3,16 @@ describe('template spec', () => {
     cy.visit('/');
     cy.get('[data-cy="today"]').should("exist");
   });
+
+  it("downloads as a png", () => {
+    cy.visit('/');
+    cy.get('[data-cy="png-download-btn"]').click();
+    cy.readFile(`cypress/downloads/today.png`).should('exist');
+  });
+
+  it("downloads as a pdf", () => {
+    cy.visit('/');
+    cy.get('[data-cy="pdf-download-btn"]').click();
+    cy.readFile(`cypress/downloads/today.pdf`).should('exist');
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,62 @@
 import { Temporal } from "@js-temporal/polyfill";
 import "./styles.css";
+import { useRef } from "react";
 
 export default function App() {
   const todaysDate = Temporal.Now.plainDateISO();
+  const todaysDateRef = useRef<HTMLHeadingElement>(null);
+
+  const downloadAsPng = async () => {
+    try {
+      const { default: html2canvas } = await import('html2canvas');
+      const canvas = await html2canvas(todaysDateRef.current as HTMLHeadingElement, {
+        width: 300,
+        scale: 1,
+        height: 200
+      });
+      const pngURL = canvas.toDataURL('image/png');
+      const dummyDoc = document.createElement('a');
+      dummyDoc.href = pngURL;
+      const pngTitle = `today.png`;
+      dummyDoc.download = pngTitle;
+      dummyDoc.click();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const downloadAsPdf = async () => {
+    try {
+      const { default: html2canvas } = await import('html2canvas');
+      const canvas = await html2canvas(todaysDateRef.current as HTMLHeadingElement, {
+        width: 300,
+        scale: 1,
+        height: 200
+      });
+      const pngURL = canvas.toDataURL('image/png');
+
+      const { jsPDF } = await import('jspdf');
+      const pdf = new jsPDF({
+        format: [canvas.width, canvas.height],
+        orientation: 'landscape',
+        unit: 'px'
+      });
+      pdf.addImage(pngURL, 'PNG', 0, 0, canvas.width, canvas.height);
+      const pdfTitle = `today.pdf`;
+      pdf.save(pdfTitle);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div className="App">
       <h1>Today is:</h1>
-      <h2 data-cy="today">
-        {todaysDate.month}/{todaysDate.day}/{todaysDate.year}
+      <h2 data-cy="today" ref={todaysDateRef}>
+        <span>{todaysDate.month}/{todaysDate.day}/{todaysDate.year}</span>
       </h2>
+      <button data-cy="png-download-btn" onClick={downloadAsPng}>Download date as PNG</button>
+      <button data-cy="pdf-download-btn" onClick={downloadAsPdf}>Download date as PDF</button>
     </div>
   );
 }
